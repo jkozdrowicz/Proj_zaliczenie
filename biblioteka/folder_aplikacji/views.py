a3bad9dd83eae9c92070933d36a59cb4bff889e3
jkozdrowicz <jkozdrowicz1@st.swps.edu.pl>
2025-01-25 19:47:32 +0100

    Autoryzacja_problemy

--- biblioteka/folder_aplikacji/views.py
+++ biblioteka/folder_aplikacji/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Autor, Czytelnik, Kategoria, Ksiazka, Wypozyczenie
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -13,138 +13,184 @@ from .serializers import AutorSerializer, CzytelnikSerializer, KategoriaSerializ
 @api_view(['GET', 'POST'])
 def kategoria_list(request):
     if request.method == 'GET':
-        osoby = Kategoria.objects.all()
-        serializer = KategoriaSerializer(osoby, many = True)
-        return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = KategoriaSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        kategorie = Kategoria.objects.all()  # Pobieramy wszystkie kategorie
+        serializer = KategoriaSerializer(kategorie, many=True)  # Serializujemy dane
+        return Response(serializer.data)  # Zwracamy dane w odpowiedzi
 
-@api_view(['GET', 'DELETE'])    
-def kategoria_details(request, pk):
-    try:
-        kategoria = Kategoria.objects.get(pk=pk)
-    except Kategoria.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer = KategoriaSerializer(kategoria)
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
-        kategoria.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+    elif request.method == 'POST':
+        serializer = KategoriaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Zapisujemy nową kategorię
+            return Response(serializer.data, status=status.HTTP_201_CREATED)  # Zwracamy dane i status 201
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # W przypadku błędu walidacji
 
 @api_view(['GET', 'POST'])
 def autor_list(request):
     if request.method == 'GET':
-        osoby = Autor.objects.all()
-        serializer = AutorSerializer(osoby, many = True)
+        autorzy = Autor.objects.all()  # Pobieramy wszystkich autorów
+        serializer = AutorSerializer(autorzy, many=True)  # Serializujemy dane
+        return Response(serializer.data)  # Zwracamy dane w odpowiedzi
+
+    elif request.method == 'POST':
+        serializer = AutorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Zapisujemy nowego autora
+            return Response(serializer.data, status=status.HTTP_201_CREATED)  # Zwracamy dane i status 201
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # W przypadku błędu walidacji
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def kategoria_details(request, pk):
+    try:
+        kategoria = Kategoria.objects.get(pk=pk)
+    except Kategoria.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = KategoriaSerializer(kategoria)
         return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = AutorSerializer(data = request.data)
+
+    elif request.method == "PUT":
+        serializer = KategoriaSerializer(kategoria, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'DELETE'])    
+    elif request.method == 'DELETE':
+        kategoria.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def autor_details(request, pk):
     try:
         autor = Autor.objects.get(pk=pk)
     except Autor.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
-    
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == "GET":
         serializer = AutorSerializer(autor)
         return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = AutorSerializer(autor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
-        Autor.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
-    
+        autor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET', 'POST'])
 def ksiazka_list(request):
     if request.method == 'GET':
-        osoby = Ksiazka.objects.all()
-        serializer = KsiazkaSerializer(osoby, many = True)
+        ksiazki = Ksiazka.objects.all()
+        serializer = KsiazkaSerializer(ksiazki, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = KsiazkaSerializer(data = request.data)
+
+    elif request.method == 'POST':
+        serializer = KsiazkaSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'DELETE'])    
+@api_view(['GET', 'PUT', 'DELETE'])
 def ksiazka_details(request, pk):
     try:
         ksiazka = Ksiazka.objects.get(pk=pk)
     except Ksiazka.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
-    
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == "GET":
         serializer = KsiazkaSerializer(ksiazka)
         return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = KsiazkaSerializer(ksiazka, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         ksiazka.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def czytelnik_list(request):
     if request.method == 'GET':
-        osoby = Czytelnik.objects.all()
-        serializer = CzytelnikSerializer(osoby, many = True)
+        czytelnicy = Czytelnik.objects.all()
+        serializer = CzytelnikSerializer(czytelnicy, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = CzytelnikSerializer(data = request.data)
+
+    elif request.method == 'POST':
+        serializer = CzytelnikSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'DELETE'])    
+@api_view(['GET', 'PUT', 'DELETE'])
 def czytelnik_details(request, pk):
     try:
         czytelnik = Czytelnik.objects.get(pk=pk)
     except Czytelnik.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
-    
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == "GET":
         serializer = CzytelnikSerializer(czytelnik)
         return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = CzytelnikSerializer(czytelnik, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         czytelnik.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
-    
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET', 'POST'])
 def wypozyczenie_list(request):
     if request.method == 'GET':
-        osoby = Wypozyczenie.objects.all()
-        serializer = WypozyczenieSerializer(osoby, many = True)
+        wypozyczenia = Wypozyczenie.objects.all()
+        serializer = WypozyczenieSerializer(wypozyczenia, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = WypozyczenieSerializer(data = request.data)
+
+    elif request.method == 'POST':
+        serializer = WypozyczenieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'DELETE'])    
+@api_view(['GET', 'PUT', 'DELETE'])
 def wypozyczenie_details(request, pk):
     try:
         wypozyczenie = Wypozyczenie.objects.get(pk=pk)
     except Wypozyczenie.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
-    
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == "GET":
         serializer = WypozyczenieSerializer(wypozyczenie)
         return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = WypozyczenieSerializer(wypozyczenie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         wypozyczenie.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
-    
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Widok powitalny
 def welcome_view(request):
     now = datetime.now()
     html = f"""
@@ -154,20 +200,3 @@ def welcome_view(request):
         </body></html>"""
     return HttpResponse(html)
 
-def ksiazki_list_html(request):
-    # pobieramy wszystkie obiekty ksiazki z bazy poprzez QuerySet
-    ksiazkis = Ksiazka.objects.all()
-    return render(request,
-                  "folder_aplikacji/ksiazki/list.html",
-                  {'ksiazkas': ksiazkis})
-
-def ksiazki_detail_html(request, id):
-    # pobieramy konkretny obiekt ksiazki
-    try:
-        ksiazki = Ksiazka.objects.get(id=id)
-    except Ksiazka.DoesNotExist:
-        raise Http404("Obiekt ksiazki o podanym id nie istnieje")
-
-    return render(request,
-                  "folder_aplikacji/ksiazki/detail.html",
-                  {'ksiazka': ksiazki})
