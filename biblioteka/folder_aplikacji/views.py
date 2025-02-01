805056555f8d06f74e0ec8d5eaa540e5e4e942cb
Wojtek <wstepniewski@st.swps.edu.pl>
2025-02-01 11:27:49 +0100

    final

--- biblioteka/folder_aplikacji/views.py
+++ biblioteka/folder_aplikacji/views.py
@@ -1,196 +1,287 @@
 from datetime import datetime
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Autor, Czytelnik, Kategoria, Ksiazka, Wypozyczenie
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login
 
 from .serializers import AutorSerializer, CzytelnikSerializer, KategoriaSerializer, KsiazkaSerializer, WypozyczenieSerializer
 
-@api_view(['GET', 'POST'])
-def kategoria_list(request):
-    if request.method == 'GET':
-        kategorie = Kategoria.objects.all()  # Pobieramy wszystkie kategorie
-        serializer = KategoriaSerializer(kategorie, many=True)  # Serializujemy dane
-        return Response(serializer.data)  # Zwracamy dane w odpowiedzi
-
-    elif request.method == 'POST':
-        serializer = KategoriaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()  # Zapisujemy nową kategorię
-            return Response(serializer.data, status=status.HTTP_201_CREATED)  # Zwracamy dane i status 201
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # W przypadku błędu walidacji
-
-@api_view(['GET', 'POST'])
-def autor_list(request):
-    if request.method == 'GET':
-        autorzy = Autor.objects.all()  # Pobieramy wszystkich autorów
-        serializer = AutorSerializer(autorzy, many=True)  # Serializujemy dane
-        return Response(serializer.data)  # Zwracamy dane w odpowiedzi
-
-    elif request.method == 'POST':
-        serializer = AutorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()  # Zapisujemy nowego autora
-            return Response(serializer.data, status=status.HTTP_201_CREATED)  # Zwracamy dane i status 201
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # W przypadku błędu walidacji
-
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])    
 def kategoria_details(request, pk):
+    try:
+        kategoria = Kategoria.objects.get(pk=pk)
+    except Kategoria.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        kategoria = Kategoria.objects.get(pk=pk)
+        serializer = KategoriaSerializer(kategoria)
+        return Response(serializer.data)
+
+@api_view(['GET'])
+def kategoria_list_get(request):
+    kategorie = Kategoria.objects.all()
+    serializer = KategoriaSerializer(kategorie, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def kategoria_list_post(request):
+    serializer = KategoriaSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def kategoria_update(request, pk):
     try:
         kategoria = Kategoria.objects.get(pk=pk)
     except Kategoria.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == "GET":
-        serializer = KategoriaSerializer(kategoria)
-        return Response(serializer.data)
-
-    elif request.method == "PUT":
+    if request.method == 'PUT':
         serializer = KategoriaSerializer(kategoria, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+@api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])    
+def kategoria_delete(request, pk): 
+    try:
+        kategoria = Kategoria.objects.get(pk=pk)
+    except Kategoria.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
         kategoria.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)   
 
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])    
 def autor_details(request, pk):
     try:
         autor = Autor.objects.get(pk=pk)
     except Autor.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
+    
     if request.method == "GET":
         serializer = AutorSerializer(autor)
         return Response(serializer.data)
 
-    elif request.method == "PUT":
+@api_view(['GET'])
+def autor_list_get(request):
+    autorzy = Autor.objects.all()
+    serializer = AutorSerializer(autorzy, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def autor_list_post(request):
+    serializer = AutorSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def autor_update(request, pk):
+    try:
+        autor = Autor.objects.get(pk=pk)
+    except Autor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
         serializer = AutorSerializer(autor, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+@api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])    
+def autor_delete(request, pk): 
+    try:
+        autor = Autor.objects.get(pk=pk)
+    except Autor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
         autor.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'POST'])
-def ksiazka_list(request):
-    if request.method == 'GET':
-        ksiazki = Ksiazka.objects.all()
-        serializer = KsiazkaSerializer(ksiazki, many=True)
-        return Response(serializer.data)
+    
+@api_view(['GET'])
+def czytelnik_list_get(request):
+    czytelnicy = Czytelnik.objects.all()
+    serializer = CzytelnikSerializer(czytelnicy, many=True)
+    return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = KsiazkaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['POST'])
+def czytelnik_list_post(request):
+    serializer = CzytelnikSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def ksiazka_details(request, pk):
-    try:
-        ksiazka = Ksiazka.objects.get(pk=pk)
-    except Ksiazka.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == "GET":
-        serializer = KsiazkaSerializer(ksiazka)
-        return Response(serializer.data)
-
-    elif request.method == "PUT":
-        serializer = KsiazkaSerializer(ksiazka, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        ksiazka.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'POST'])
-def czytelnik_list(request):
-    if request.method == 'GET':
-        czytelnicy = Czytelnik.objects.all()
-        serializer = CzytelnikSerializer(czytelnicy, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = CzytelnikSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
+@api_view(['GET'])
 def czytelnik_details(request, pk):
     try:
         czytelnik = Czytelnik.objects.get(pk=pk)
     except Czytelnik.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = CzytelnikSerializer(czytelnik)
+    return Response(serializer.data)
 
-    if request.method == "GET":
-        serializer = CzytelnikSerializer(czytelnik)
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def czytelnik_update(request, pk):
+    try:
+        czytelnik = Czytelnik.objects.get(pk=pk)
+    except Czytelnik.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CzytelnikSerializer(czytelnik, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "PUT":
-        serializer = CzytelnikSerializer(czytelnik, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def czytelnik_delete(request, pk):
+    try:
+        czytelnik = Czytelnik.objects.get(pk=pk)
+    except Czytelnik.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    elif request.method == 'DELETE':
-        czytelnik.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    czytelnik.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'POST'])
-def wypozyczenie_list(request):
-    if request.method == 'GET':
-        wypozyczenia = Wypozyczenie.objects.all()
-        serializer = WypozyczenieSerializer(wypozyczenia, many=True)
+# KSIAZKI
+@api_view(['GET'])
+def ksiazka_list_get(request):
+    ksiazki = Ksiazka.objects.all()
+    serializer = KsiazkaSerializer(ksiazki, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def ksiazka_list_post(request):
+    serializer = KsiazkaSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def ksiazka_details(request, pk):
+    try:
+        ksiazka = Ksiazka.objects.get(pk=pk)
+    except Ksiazka.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = KsiazkaSerializer(ksiazka)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def ksiazka_update(request, pk):
+    try:
+        ksiazka = Ksiazka.objects.get(pk=pk)
+    except Ksiazka.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = KsiazkaSerializer(ksiazka, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'POST':
-        serializer = WypozyczenieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def ksiazka_delete(request, pk):
+    try:
+        ksiazka = Ksiazka.objects.get(pk=pk)
+    except Ksiazka.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET', 'PUT', 'DELETE'])
+    ksiazka.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# WYPOŻYCZENIA
+@api_view(['GET'])
+def wypozyczenie_list_get(request):
+    wypozyczenia = Wypozyczenie.objects.all()
+    serializer = WypozyczenieSerializer(wypozyczenia, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def wypozyczenie_list_post(request):
+    serializer = WypozyczenieSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
 def wypozyczenie_details(request, pk):
     try:
         wypozyczenie = Wypozyczenie.objects.get(pk=pk)
     except Wypozyczenie.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = WypozyczenieSerializer(wypozyczenie)
+    return Response(serializer.data)
 
-    if request.method == "GET":
-        serializer = WypozyczenieSerializer(wypozyczenie)
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def wypozyczenie_update(request, pk):
+    try:
+        wypozyczenie = Wypozyczenie.objects.get(pk=pk)
+    except Wypozyczenie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = WypozyczenieSerializer(wypozyczenie, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "PUT":
-        serializer = WypozyczenieSerializer(wypozyczenie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['DELETE'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def wypozyczenie_delete(request, pk):
+    try:
+        wypozyczenie = Wypozyczenie.objects.get(pk=pk)
+    except Wypozyczenie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    elif request.method == 'DELETE':
-        wypozyczenie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    wypozyczenie.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
-# Widok powitalny
 def welcome_view(request):
     now = datetime.now()
     html = f"""
@@ -200,3 +291,50 @@ def welcome_view(request):
         </body></html>"""
     return HttpResponse(html)
 
+
+def ksiazki_list_html(request):
+    # pobieramy wszystkie obiekty ksiazki z bazy poprzez QuerySet
+    ksiazkis = Ksiazka.objects.all()
+    return render(request,
+                  "folder_aplikacji/ksiazki/list.html",
+                  {'ksiazkas': ksiazkis})
+
+def ksiazki_detail_html(request, id):
+    # pobieramy konkretny obiekt ksiazki
+    try:
+        ksiazki = Ksiazka.objects.get(id=id)
+    except Ksiazka.DoesNotExist:
+        raise Http404("Obiekt ksiazki o podanym id nie istnieje")
+
+    return render(request,
+                  "folder_aplikacji/ksiazki/detail.html",
+                  {'ksiazka': ksiazki})
+    
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Error creating your account')
+    else:
+        form = UserCreationForm()
+    return render(request, 'folder_aplikacji\\rejestracja\\rejestracja.html', {'form': form})
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({'error': 'Please provide username and password.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+        return redirect('home')
